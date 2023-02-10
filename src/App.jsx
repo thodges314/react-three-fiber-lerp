26d2ae829c8b09d0ebb5d57e7606c303cb7a505b
Thomas Hodges <thodges@javascriptonline.org>
2023-02-10 13:34:49 -0800

    LERP part 1

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import { Canvas, useThree, useFrame } from "@react-three/fiber";
+import { Vector3 } from "three";
+import { Stats, Environment, Center } from "@react-three/drei";
+import Button from "./Button";
+
+// const vec = new Vector3();
+
+// const Rig = () => {
+//   return useFrame(({ camera, mouse }) => {
+//     vec.set(mouse.x * 2, mouse.y * 2, camera.position.z);
+//     camera.position.lerp(vec, 0.025);
+//     camera.lookAt(0, 0, 0);
+//   });
+// };
+
+const Rig = () => {
+  const vec = new Vector3();
+  useFrame(({ camera, mouse }) => {
+    vec.set(mouse.x * 2, mouse.y * 2, camera.position.z);
+    camera.position.lerp(vec, 0.025);
+    camera.lookAt(0, 0, 0);
+  });
+};
+const App = () => (
+  <Canvas camera={{ position: [0, 0, 5] }}>
+    <Environment preset="forest" background />
+    <Center>
+      {[...Array(5).keys()].map((x) =>
+        [...Array(3).keys()].map((y) => (
+          <Button key={x + y * 5} position={[x * 2.5, y * 2.5, 0]} />
+        ))
+      )}
+    </Center>
+    <Rig />
+    <Stats />
+  </Canvas>
+);
+
+export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { Canvas } from "@react-three/fiber";
-import { Stats } from "@react-three/drei";
 
 import "./styles.css";
 
+import App from "./App";
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Canvas camera={{ position: [0, 0, 2] }}>
-      <mesh>
-        <boxGeometry />
-        <meshBasicMaterial color={0x00ff00} wireframe />
-        <Stats />
-      </mesh>
-    </Canvas>
+    <App />
   </StrictMode>
 );
